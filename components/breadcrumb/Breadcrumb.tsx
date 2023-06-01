7dcedb4f6239c56b30f54acf5595ad14310180ea
Kodir Amirov <amirovqodir911@gmail.com>
2023-06-01 21:33:10 +0900

    breadcrumb

--- components/breadcrumb/Breadcrumb.tsx
+++ components/breadcrumb/Breadcrumb.tsx
@@ -1,15 +1,38 @@
+'use client'
+import { usePathname } from "next/navigation";
+
 import { FC } from "react";
 
 import './styles.css';
+import Link from "next/link";
 
 export const Breadcrumb: FC = () => {
+    const currentUrl = usePathname();
+    const currentRotes = currentUrl.split('/');
+
+    const getPath = (index: number) => currentUrl.split('/').slice(0, index + 1).join('/')
+
+    const createRoutes = () => {
+        currentRotes
+        const routes: any = []
+        for(let i = 1; i <= currentRotes.length; i++){
+            if(i === 1) {
+                routes.push(<Link key={i} href={getPath(i)} className="active-route">Home</Link>)
+            } else {
+                routes.push(<Link key={i} href={getPath(i)} className={i !== currentRotes.length - 1 ? 'active-route capitalize': 'capitalize'}>
+                    {currentRotes[i]}</Link>)
+            }
+
+            if(i < currentRotes.length - 1) {
+                routes.push(<span key={i+10}> / </span>)
+            }
+        }
+        return routes;
+    }
+
     return <div className="breadcrumb rounded flex items-center text-base font-medium">
         <div className="routes ml-3">
-            <span className="active-route route">Home</span>
-            <span> / </span>
-            <span className="active-route route">Search</span>
-            <span> / </span>
-            <span className="route">Result</span>
+            {createRoutes()}
         </div>
     </div>
 }
