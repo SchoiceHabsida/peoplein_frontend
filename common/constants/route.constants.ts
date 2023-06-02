c52a422e5a41fd1d5efcb0da3033e27081855ad6
Kodir Amirov <amirovqodir911@gmail.com>
2023-06-02 20:17:59 +0900

    applicants

--- common/constants/route.constants.ts
+++ common/constants/route.constants.ts
@@ -1,6 +1,6 @@
 export const ROUTE_HOME = '';
 export const ROUTE_LOGIN = '/login';
-export const ROUTE_APPLICANTS = '/dashboard';
+export const ROUTE_APPLICANTS = '/applicants';
 export const ROUTE_SEARCH = '/search';
 export const ROUTE_FAVORITES = '/favorites';
 export const ROUTE_INTERVIEWS = '/interviews';
