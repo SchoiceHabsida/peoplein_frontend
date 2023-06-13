1b188303d3d8744ae4f531bd89963c00832c3892
Kodir Amirov <amirovqodir911@gmail..com>
2023-06-13 19:06:43 +0900

    company detail

--- common/components/models/companies.model.ts
+++ common/components/models/companies.model.ts
@@ -1,7 +1,12 @@
+import { ISkills } from "./applicants.model"
+
 export interface ICompany {
     id?: string,
     name: string,
     address: string,
     hrManager: string
     phoneNumber: string,
+    skills?: ISkills[],
+    email?: string,
+    favoriteApplicants?: {lastName: string, firstName: string }[]
 }
--- components/company-details/CompanyDetails.tsx
+++ components/company-details/CompanyDetails.tsx
@@ -5,10 +5,27 @@ import { ActionButtonWrapper } from "../admin-status-actions/AdminStatusActions"
 import './styles.css';
 import { usePathname, useRouter } from "next/navigation";
 import { FC } from "react";
+import { gql, useQuery } from "@apollo/client";
+import { ICompany } from "@/common/components/models/companies.model";
 
-export const CompanyDetails: FC<{id: string}> = ({id}) => {
+const GET_COMPANY_QUERY = gql`
+query getCompany ($id: ID!) {
+    getCompanyById (id: $id) {
+        id
+        name
+        address 
+        hrManager
+        favoriteApplicants {
+            firstName
+            lastName
+        }
+    }
+}
+`
 
-    const mock_id = '1';
+export const CompanyDetails: FC<{ id: string }> = ({ id }) => {
+    const { data, loading } = useQuery<{ 'getCompanyById': ICompany }>(GET_COMPANY_QUERY, { variables: { id: id } })
+
     const path = usePathname();
     const router = useRouter();
 
@@ -20,7 +37,7 @@ export const CompanyDetails: FC<{id: string}> = ({id}) => {
             <div className="right-actions flex flex-col flex-grow items-end justify-center gap-2 pr-6">
 
                 <ActionButtonWrapper
-                    onCLick={() => router.push(`${path}/${mock_id}`)}
+                    onCLick={() => router.push(`${path}/${id}`)}
                     label={'Edit company'}
                     icon={<ArrowRight></ArrowRight>} />
             </div>
@@ -28,7 +45,7 @@ export const CompanyDetails: FC<{id: string}> = ({id}) => {
         <div className="flex gap-2">
             <div className="w-2/3">
                 <div className="mb-14">
-                    <div className="applicant-name">Company</div>
+                    <div className="applicant-name">{data?.getCompanyById.name}</div>
                     <div>Registration number</div>
                 </div>
             </div>
@@ -36,21 +53,23 @@ export const CompanyDetails: FC<{id: string}> = ({id}) => {
                 <div className="sub-title">
                     Skills:
                 </div>
+                {
+                    data?.getCompanyById.skills?.map((skill, index) => <div key={index}> {skill.skillName} , </div>)
+                }
                 <div className="mb-6">
-                    Java, JS, React</div>
+                </div>
                 <div className="sub-title">
                     Email:
                 </div>
-                <div className="mb-6">main@mail.com</div>
+                <div className="mb-6">{data?.getCompanyById?.email}</div>
                 <div className="sub-title">
                     Contact number:
                 </div>
-                <div className="mb-6">010 1234 5566 78</div>
+                <div className="mb-6">{data?.getCompanyById?.phoneNumber}</div>
                 <div className="sub-title">
-                    In favorites:
+                    Favorite user:
                 </div>
-                <div>1) Name A</div>
-                <div>2) Name B</div>
+                {data?.getCompanyById.favoriteApplicants?.map((applicant, index) => <div key={index}>{index + 1}) {applicant.firstName} {applicant.lastName} </div>)}
             </div>
         </div>
     </div>
