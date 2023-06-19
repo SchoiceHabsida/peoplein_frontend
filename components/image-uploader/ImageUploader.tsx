bccb5f421051c38a411b0adeee0503f26ee7ebd8
Kodir Amirov <amirovqodir911@gmail..com>
2023-06-20 01:38:58 +0900

    add resume and user photo

--- components/image-uploader/ImageUploader.tsx
+++ components/image-uploader/ImageUploader.tsx
@@ -12,11 +12,13 @@ export const ImageUploader: React.FC<{ onChange: (file: File) => void, imagePath
 
   return (
     <div className='w-full h-full absolute'>
-      <input type="file" className='cursor-pointer absolute opacity-0 w-full h-full z-20' onChange={handleFileChange} />
+      <input type="file" className='cursor-pointer absolute opacity-0 w-full h-full z-20'
+        onChange={handleFileChange} />
       <div className='w-full h-full top-0 absolute z-10 overflow-hidden'>
         {(selectedFile || imagePath) && (
-          <img className='w-full h-full' src={selectedFile ? URL.createObjectURL(selectedFile) : imagePath} alt="Selected" />
-         )} 
+          <img className='w-full h-full'
+            src={selectedFile ? URL.createObjectURL(selectedFile) : imagePath} alt="Selected" />
+        )}
       </div>
     </div>
   );
